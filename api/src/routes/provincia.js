6a5b9dbeca95da62cd133e9dd7fccf8b020158c5
Giulia Barber <86272025+jubarber@users.noreply.github.com>
2022-03-26 20:48:57 -0300

    ruteo completo con vehiculo, FALTA RUTEO VIAJES (#2)
    * ruteo completo con vehiculo, FALTA RUTEO VIAJES

--- api/src/routes/provincia.js
+++ api/src/routes/provincia.js
@@ -1,3 +1,48 @@
 const { Router } = require("express");
 const router = Router();
+
+const { Provincia } = require("../db.js");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const provinciasArray = [
+      "Tierra del Fuego",
+      "Santa Cruz",
+      "Chubut",
+      "Río Negro",
+      "Neuquen",
+      "La Pampa",
+      "Mendoza",
+      "San Juan",
+      "San Luis",
+      "Córdoba",
+      "Buenos Aires",
+      "La Rioja",
+      "Catamarca",
+      "Tucumán",
+      "Santiago del Estero",
+      "Santa Fe",
+      "Entre Ríos",
+      "Corrientes",
+      "Misiones",
+      "Chaco",
+      "Formosa",
+      "Salta",
+      "Jujuy"
+    ];
+
+    provinciasArray.forEach((e, index) => {
+      e &&
+        Provincia.findOrCreate({
+          where: { nombre: e, id: index + 1 }
+        });
+    });
+  const provincias = await Provincia.findAll({});
+  res.send(provincias);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
+
