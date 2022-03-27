87aa1d4f62028480c29c5bae98942749e06834d3
Ignacio Freilij <ignacio.freilij@gmail.com>
2022-03-27 17:24:51 -0300

    post viaje

--- api/src/routes/viaje.js
+++ api/src/routes/viaje.js
@@ -1,3 +1,22 @@
 const { Router } = require("express");
 const router = Router();
+const {Viaje} = require("../db.js")
+
+
+router.post("/", async (req, res,next) => {
+    try {
+        const {fecha, hora, origen, destino, asientosDisponibles,formaDePago,pagoCompartido,aceptaFumador, aceptaMascota,usaBarbijo, aceptaEquipaje, viajeDisponible} = req.body
+    let nuevoViaje
+     if(fecha&&origen&&destino){
+         nuevoViaje = await Viaje.findOrCreate({
+             where:{
+                 fecha, hora, origen, destino, asientosDisponibles,formaDePago,pagoCompartido,aceptaEquipaje,aceptaFumador,aceptaMascota,usaBarbijo,viajeDisponible
+             }
+         })
+        res.json(nuevoViaje)
+    }
+ } catch (error) {
+        next(error)
+    }
+})
 module.exports = router;
