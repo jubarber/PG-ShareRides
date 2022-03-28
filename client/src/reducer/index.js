547e88946e473d110c6bc6836947c166b70e83ea
Giulia Barber <86272025+jubarber@users.noreply.github.com>
2022-03-28 14:51:51 -0300

    componente detalle de viaje funcional, accion getDetalleViaje y reducer (#15)
    * Ãrutas get inicio sesion y post register funcionales para usuarios SIN vehiculo
    
    * ruteo completo con vehiculo, FALTA RUTEO VIAJES
    
    * post viaje relacionado con usuario
    
    * rutas get viaje y get viaje por ID terminadas
    
    * post viaje pasajero terminada, modelo viaje actualizado
    
    * post viaje pasajero terminada, modelo viaje modificado
    
    * put modificacion contraseña
    
    * put cambio password
    
    * componente detalle de viaje funcional, accion getDetalleViaje y reducer
    
    * componente detalle de viaje funcional, accion getDetalleViaje y reducer

--- client/src/reducer/index.js
+++
@@ -1,9 +0,0 @@
-const initialState = {
-    sinuso:[]
-}
-function rootReducer(state=initialState, action){
-    switch(action.type){
-        default: return {...state}
-    }
-}
-export default rootReducer
