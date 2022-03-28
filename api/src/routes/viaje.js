3590a03633793e9d8ad145bb9b30f20cf866e318
gbarber <julibarber22@gmail.com>
2022-03-28 11:13:18 -0300

    post viaje pasajero terminada, modelo viaje actualizado

--- api/src/routes/viaje.js
+++ api/src/routes/viaje.js
@@ -2,14 +2,14 @@ const { Router } = require("express");
 const router = Router();
 const { Viaje, Usuario } = require("../db.js");
 
-router.post("/", async (req, res, next) => {
+router.post("/conductor", async (req, res, next) => {
   try {
     const {
       fecha,
       hora,
       origen,
       destino,
-      asientosDisponibles,
+      asientosAOcupar,
       formaDePago,
       pagoCompartido,
       aceptaFumador,
@@ -27,7 +27,7 @@ router.post("/", async (req, res, next) => {
         hora,
         origen,
         destino,
-        asientosDisponibles,
+        asientosAOcupar,
         formaDePago,
         pagoCompartido,
         aceptaEquipaje,
@@ -39,6 +39,48 @@ router.post("/", async (req, res, next) => {
       await nuevoViaje.addUsuario(dni);
       usuarioConductor.update({ conductor: true });
       usuarioConductor.save();
+
+      res.json(nuevoViaje);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/pasajero", async (req, res, next) => {
+  try {
+    const {
+      fecha,
+      hora,
+      origen,
+      destino,
+      asientosAOcupar,
+      formaDePago,
+      pagoCompartido,
+      aceptaFumador,
+      aceptaMascota,
+      usaBarbijo,
+      aceptaEquipaje,
+      viajeDisponible,
+      dni
+    } = req.body;
+
+    if (fecha && origen && destino) {
+      var nuevoViaje = await Viaje.create({
+        fecha,
+        hora,
+        origen,
+        destino,
+        asientosAOcupar,
+        formaDePago,
+        pagoCompartido,
+        aceptaEquipaje,
+        aceptaFumador,
+        aceptaMascota,
+        usaBarbijo,
+        viajeDisponible
+      });
+      await nuevoViaje.addUsuario(dni);
 
       res.json(nuevoViaje);
     }
