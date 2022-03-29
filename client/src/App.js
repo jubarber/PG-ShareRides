15e29c1bf60c8765fe931c1b851cb41b977ced7e
Julio Humere <92033293+Julihumere@users.noreply.github.com>
2022-03-29 11:21:59 -0300

    Julio (#17)
    * Instalaciones
    
    * Tailwind
    
    * Mui
    
    * Landing 80%
    
    * No quiero sufrir mas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/Landing/LandingPage";
-import LandingPage from "./components/LandingPage";
 import { Home } from "./components/Home";
-
 
 function App() {
   return (
