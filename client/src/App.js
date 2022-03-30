3fde51787aa560e9c18d0e6ad52d290520896cb5
Julio Humere <92033293+Julihumere@users.noreply.github.com>
2022-03-30 18:39:28 -0300

    Julio (#28)
    * Instalaciones
    
    * Tailwind
    
    * Mui
    
    * Landing 80%
    
    * No quiero sufrir mas
    
    * vamo a rompe
    
    * Perfil. Estilos terminados. Falta unir logica
    
    * Perfil. Estilos terminado, falta logica

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
-
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/Landing/LandingPage";
 import { Home } from "./components/Home";
 import Perfil from "./components/Perfil/Perfil";
 import FormPerfil from "./components/FormPerfil/FormPerfil.jsx";
-import FormRegistro from './components/FormRegistro/FormRegistro';
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -17,9 +14,6 @@ function App() {
           <Route exact path="/home" element={<Home />} />
           <Route exact path="/perfil" element={<Perfil />} />
           <Route exact path="/perfil/editar" element={<FormPerfil />} />
-          <Route exact path="/registro" element={<FormRegistro />} />
-
-
         </Routes>
       </BrowserRouter>
     </div>
