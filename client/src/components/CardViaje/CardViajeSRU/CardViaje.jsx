6be80c39f24b7779ddb4c8de576934d553f20141
Gabriel Maguna <gabimaguna@gmail.com>
2022-03-31 17:00:41 -0300

    Dinamica a las cards. GM

--- client/src/components/CardViaje/CardViajeSRU/CardViaje.jsx
+++ client/src/components/CardViaje/CardViajeSRU/CardViaje.jsx
@@ -1,42 +1,71 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import "./CardViaje.css"
+import { Link } from "react-router-dom";
+import "./CardViaje.css";
 import { VscLocation } from "react-icons/vsc";
-import {MdSmokeFree, MdMasks, MdPets} from "react-icons/md";
-import {FaSuitcaseRolling} from "react-icons/fa"
+import { MdSmokeFree, MdMasks, MdPets } from "react-icons/md";
+import { FaSuitcaseRolling } from "react-icons/fa";
 
-
-export default function Card ({title, image, diets,spoonacularScore, dishTypes, createdInDB}){
-
-    
-    const handleClick=(e)=>{
-
-    }
-    return (
-        <div className="container-card font-mono italic">   
-        <div className="data-card">
-            <div className="prueba">
-                <i className="text-orange-600 text-xs text-left w-full flex flex-col-reverse w-4/12"><VscLocation className="icono text-purple-600"/>origen</i><i className="text-base w-full">CABA </i>               
-            </div>
-                <i className="text-right w-full text-sm	">Buenos Aires</i>            
-            <div className="prueba">
-                <i className="text-sky-400 text-xs text-left w-full flex flex-col-reverse w-4/12"><VscLocation className="icono text-purple-600"/>destino</i><i className="text-base w-full">Mar del Plata </i>               
-            </div>
-                <i className="text-right w-full text-sm	">Buenos Aires</i>
-                <h5 className="text-center text-xs">
-            14:00 hs <p className="text-right text-xs">28-12-2022</p>
-          </h5>                        <div className="dos"><h5 className="text-left">x lugares libre</h5></div>        </div>     
-        <div className="icon-card">
-            <div className="iconos">
-                <MdPets/>
-                <MdSmokeFree />
-                <FaSuitcaseRolling />
-                <MdMasks />
-            </div>
-            <Link to={"/"} id="ver"><div><button className="ver-button">Ver</button></div></Link>
-        </div> 
- 
-            
+export default function Card({
+  origen,
+  destino,
+  fecha,
+  hora,
+  asientosAOcupar,
+  aceptaEquipaje,
+  aceptaFumador,
+  aceptaMascota,
+  usaBarbijo,
+  viajeDisponible,
+}) {
+  return (
+    <div className="container-card font-mono italic">
+      <div className="data-card">
+        <div className="prueba">
+          <i className="text-orange-600 text-xs text-left w-full flex flex-col-reverse w-4/12">
+            <VscLocation className="icono text-purple-600" />
+            origen
+          </i>
+          <i className="text-base w-full">{origen}</i>
         </div>
-    )
+        <i className="text-right w-full text-sm	">{origen}</i>
+        <div className="prueba">
+          <i className="text-sky-400 text-xs text-left w-full flex flex-col-reverse w-4/12">
+            <VscLocation className="icono text-purple-600" />
+            destino
+          </i>
+          <i className="text-base w-full">{destino} </i>
+        </div>
+        <i className="text-right w-full text-sm	">{destino}</i>
+        <h5 className="text-center text-xs">
+          {hora} hs <p className="text-right text-xs">{fecha}</p>
+        </h5>
+        <div className="dos">
+          <h5 className="text-left">
+            {asientosAOcupar > 1
+              ? `${asientosAOcupar} lugares libres`
+              : asientosAOcupar === 1
+              ? `${asientosAOcupar} lugar libre`
+              : ""}
+          </h5>
+        </div>
+      </div>
+      <div className="icon-card">
+        <div className="iconos">
+          {aceptaMascota ? <MdPets /> : <></>}
+          {aceptaFumador ? <MdSmokeFree /> : <></>}
+          {aceptaEquipaje ? <FaSuitcaseRolling /> : <></>}
+          {usaBarbijo ? <MdMasks /> : <></>}
+        </div>
+        {viajeDisponible ? (
+          <Link to={"/viajes/"} id="ver">
+            <div>
+              <button className="ver-button">Ver</button>
+            </div>
+          </Link>
+        ) : (
+          <h5>Lleno</h5>
+        )}
+      </div>
+    </div>
+  );
 }
