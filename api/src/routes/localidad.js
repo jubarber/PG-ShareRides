95d2b8758d0d044d852f90e1ec9f53d6829523ea
Giulia Barber <86272025+jubarber@users.noreply.github.com>
2022-04-04 19:57:43 -0300

    ruteo localidades arreglado este sí (#74)
    * Ãrutas get inicio sesion y post register funcionales para usuarios SIN vehiculo
    
    * ruteo completo con vehiculo, FALTA RUTEO VIAJES
    
    * post viaje relacionado con usuario
    
    * rutas get viaje y get viaje por ID terminadas
    
    * post viaje pasajero terminada, modelo viaje actualizado
    
    * post viaje pasajero terminada, modelo viaje modificado
    
    * put modificacion contraseña
    
    * put cambio password
    
    * componente detalle de viaje funcional, accion getDetalleViaje y reducer
    
    * componente detalle de viaje funcional, accion getDetalleViaje y reducer
    
    * filtros no funcionales para estilar
    
    * filtros para estilar corregido
    
    * FILTROS TERMINADOS CON IGNA AL FIN
    
    * ruteo back corregido, PK usuario email
    
    * ruteo back modificado, PK usuario email
    
    * login funcionale, sin estilos
    
    * login terminado y modelos actualizados
    
    * importaciones arregladas
    
    * home y detalleViaje sueltos ELIMINADOS
    
    * modelo viaje modificado
    
    * modelo viaje actualizado con atributo detalle
    
    * modelo usuario con atributo calificacion agregado
    
    * diferenciacion viaje conductore/pasajere
    
    * ruteo localidades arreglado
    
    * ruteo localidades arreglado este sí

--- api/src/routes/localidad.js
+++ api/src/routes/localidad.js
@@ -4,41 +4,6 @@ const axios = require("axios");
 const { Localidad } = require("../db.js");
 
 router.get("/localidades", async (req, res, next) => {
-  try {
-    let localidadesDb = await Localidad.findAll();
-    let apiLocalidades;
-    if (localidadesDb.length === 0) {
-      const apiUrl = await axios.get(
-        "https://apis.datos.gob.ar/georef/api/localidades?aplanar=true&campos=estandar&max=5000&formato=json"
-      );
-      if (apiUrl) {
-        apiLocalidades = apiUrl.data.localidades.map((e) => {
-          return {
-            nombre: e.nombre,
-            municipio: e.municipio_nombre,
-            provincia: e.provincia_nombre
-          };
-        });
-      }
-      apiLocalidades.map(async (e) => {
-        await Localidad.findOrCreate({
-          where: {
-            nombre: e.nombre,
-            municipio: e.municipio,
-            provincia: e.provincia
-          }
-        });
-      });
-      res.send(apiLocalidades);
-    } else {
-      res.send(localidadesDb);
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get("/localidad", async (req, res, next) => {
   const { nombre } = req.query;
   try {
     let localidadesFiltradas;
@@ -47,6 +12,35 @@ router.get("/localidad", async (req, res, next) => {
       localidadesFiltradas = localidades.filter((e) => {
         return e.dataValues.nombre.toLowerCase().includes(nombre.toLowerCase());
       });
+    } else {
+      let localidadesDb = await Localidad.findAll();
+      let apiLocalidades;
+      if (localidadesDb.length === 0) {
+        const apiUrl = await axios.get(
+          "https://apis.datos.gob.ar/georef/api/localidades?aplanar=true&campos=estandar&max=5000&formato=json"
+        );
+        if (apiUrl) {
+          apiLocalidades = apiUrl.data.localidades.map((e) => {
+            return {
+              nombre: e.nombre,
+              municipio: e.municipio_nombre,
+              provincia: e.provincia_nombre
+            };
+          });
+        }
+        apiLocalidades.map(async (e) => {
+          await Localidad.findOrCreate({
+            where: {
+              nombre: e.nombre,
+              municipio: e.municipio,
+              provincia: e.provincia
+            }
+          });
+        });
+        res.send(apiLocalidades);
+      } else {
+        res.send(localidadesDb);
+      }
     }
     res.send(localidadesFiltradas);
   } catch (err) {
