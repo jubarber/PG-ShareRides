0b5a7c46690132c381060a6d311fb3f938fe0d9b
gbarber <julibarber22@gmail.com>
2022-04-04 19:43:39 -0300

    ruteo localidades arreglado

--- api/src/routes/localidad.js
+++ api/src/routes/localidad.js
@@ -1,66 +1,57 @@
 const { Router } = require("express");
 const router = Router();
-//const  getApiInfo = require("../controller/ApiLocalidades.js")
-const axios = require('axios')
-const { Localidad } = require('../db.js')
+const axios = require("axios");
+const { Localidad } = require("../db.js");
 
-
-const getLocalidades = async()=>{
-    const localTable = await Localidad.findAll();
-
-    if(localTable.length === 0){
-
-        try{
-            const api = await axios.get("https://apis.datos.gob.ar/georef/api/localidades?aplanar=true&campos=estandar&max=5000&formato=json");
-            const localidades = await api.data.localidades.map((l)=>{
-                return{
-                 //id: l.id,
-                 nombre: l.nombre,
-                 provincia: l.provincia_nombre,
-                 municipio: l.municipio_nombre
-                 }
-
-            })
-            localidades.map(async (e)=>{
-                await Localidad.findOrCreate({
-                    where:{
-                        nombre: e.nombre,
-                        provincia:e.provincia,
-                        municipio: e.municipio
-                    }
-                })
-            })
-            return localidades
-        } catch(error){
-         
-           console.log(error)
-          //  next(error)
-        }
-    }else{
-        return localTable
+router.get("/localidades", async (req, res, next) => {
+  try {
+    let localidadesDb = await Localidad.findAll();
+    let apiLocalidades;
+    if (localidadesDb.length === 0) {
+      const apiUrl = await axios.get(
+        "https://apis.datos.gob.ar/georef/api/localidades?aplanar=true&campos=estandar&max=5000&formato=json"
+      );
+      if (apiUrl) {
+        apiLocalidades = apiUrl.data.localidades.map((e) => {
+          return {
+            nombre: e.nombre,
+            municipio: e.municipio_nombre,
+            provincia: e.provincia_nombre
+          };
+        });
+      }
+      apiLocalidades.map(async (e) => {
+        await Localidad.findOrCreate({
+          where: {
+            nombre: e.nombre,
+            municipio: e.municipio,
+            provincia: e.provincia
+          }
+        });
+      });
+      res.send(apiLocalidades);
+    } else {
+      res.send(localidadesDb);
     }
-}
+  } catch (err) {
+    next(err);
+  }
+});
 
-
-router.get('/localidades', async (req, res)=>{
-    const { name }= req.query
-    const allLocalidades = await getLocalidades();
-    try{
-
-         if(name){
- 
-             const filtrado = allLocalidades.filter((e)=>{
-                 return e.nombre.toLowerCase().includes(name.toLowerCase())
-             })
-             if(filtrado.length) return res.status(200).send(filtrado)
-             return res.status(404).json("Localidad no existe")
-         }else{
-             return res.json(allLocalidades)
-         }
-     }catch(err){
-         err
-         console.log(err)
-     } 
-})
+router.get("/localidad", async (req, res, next) => {
+  const { nombre } = req.query;
+  try {
+    let localidadesFiltradas;
+    if (nombre) {
+      let localidades = await Localidad.findAll();
+      localidadesFiltradas = localidades.filter((e) => {
+        return e.dataValues.nombre.toLowerCase().includes(nombre.toLowerCase());
+      });
+    }
+    res.send(localidadesFiltradas);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
