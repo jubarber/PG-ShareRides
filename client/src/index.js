a81643e2bdfb91915aeb5fbd860d119bc7de5cb9
Gabriel Maguna <gabimaguna@gmail.com>
2022-04-18 17:50:14 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import { Provider } from "react-redux";
 import store from "./redux/store/store.js";
 import reportWebVitals from './reportWebVitals';
-import dotenv from "dotenv";
 import axios from "axios"
-dotenv.config();
  
 axios.defaults.baseURL = process.env.REACT_APP_API||"http://localhost:3001"
 
